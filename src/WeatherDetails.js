a0083ef01caa6d31bdd4e9a6cf74d320e7f2ca99
brnnkva <64192884+brnnkva@users.noreply.github.com>
2021-07-09 10:16:03 +0200

    added css for WeatherDetails

--- src/WeatherDetails.js
+++ src/WeatherDetails.js
@@ -3,8 +3,16 @@ import "./WeatherDetails.css";
 
 export default function WeatherDetails(){
     return (
-        <div>
-            Hola
-        </div>
-    )
+        <ul className="WeatherDetails">
+        <li>
+          weather: <span id="weather-discription">clear</span>
+        </li>
+        <li>
+          wind: <span id="wind">20</span> km/h
+        </li>
+        <li>
+          humidity: <span id="humidity">46</span> %
+        </li>
+      </ul>
+    );
 }
