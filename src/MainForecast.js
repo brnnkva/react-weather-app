303ebe376313d0e8778553f1578c5dcf7f8631f7
brnnkva <64192884+brnnkva@users.noreply.github.com>
2021-07-09 10:10:51 +0200

    added css for MainForecast

--- src/MainForecast.js
+++ src/MainForecast.js
@@ -3,8 +3,19 @@ import "./MainForecast.css";
 
 export default function MainForecast(){
     return (
-        <div>
-            hi
+        <div className="MainForecast">
+        <div className="clearfix main-forecast">
+          <img
+            src="http://openweathermap.org/img/wn/01d@2x.png"
+            alt="Clear"
+            id="main-icon"
+            className="float-left"
+          />
+          <div className="float-left">
+            <span id="temperature">9</span>
+            <small id="units">°C</small>
+          </div>
         </div>
-    )
+      </div>
+    );
 }
